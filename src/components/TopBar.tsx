cf1cb77bca1b71f6ecbaa7e3a359f8ff6dbda225
kartikav05 <kavadhani@ltvworkingteam.com>
2025-05-16 23:38:59 +0530

    fix: some changes

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -27,9 +27,6 @@ const TopBar = () => {
             <Link to="/about" className="text-gray-600 hover:text-gray-900 transition-colors">
               About
             </Link>
-            <button className="bg-blue-600 text-white px-4 py-2 rounded-lg hover:bg-blue-700 transition-colors">
-              Sign In
-            </button>
           </div>
         </div>
       </div>
